d64b56f168e8f0cb26695e739a8a86b74bc65574
Jainil Gupta <jainilgupta93@gmail.com>
2026-03-01 15:10:48 +0530

    Fix main method declaration in OOPExample.java

--- OOPExample.java
+++ OOPExample.java
@@ -9,7 +9,7 @@ class Student {
 }
 
 class Main {
-    class static void main(String[] args) {
+    public static void main(String[] args) {
 
         Student s1 = new Student();
         s1.name = "Jay";
