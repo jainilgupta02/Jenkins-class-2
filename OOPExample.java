9254a3d7678281c848d7f68f35513ff30b6505b4
Jainil Gupta <jainilgupta93@gmail.com>
2026-03-01 15:20:35 +0530

    Add study field to Student class

--- OOPExample.java
+++ OOPExample.java
@@ -1,10 +1,12 @@
 class Student {
     String name;
     int age;
+    string study
 
     void display() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
+        System.out.println("education: " + study);
     }
 }
 
@@ -14,7 +16,7 @@ public class OOPExample {
         Student s1 = new Student();
         s1.name = "Jay";
         s1.age = 21;
-
+        s1.study = "B.Tech"
         s1.display();
     }
 }
